1c99036af55a7c4d0470935857e6aff5acc7ed5c
Alice <alicegutteridge@gmail.com>
2015-02-02 22:34:16 +0000

    changed _ unknowns to Values

--- mazeSolver.pl
+++ mazeSolver.pl
@@ -1,20 +1,34 @@
 % solve(List1, List2, List3[Lots of Lists])
 
-solve(Point, [ToH|ToT], [ToH|ToT]) :- !.
+solve(From, To, To) :- 
+	isAdjacent(From, To),
+	isNotBarrier(From),
+	!.
 
 solve([FromH|FromT], To, [[FromH|FromT]|[]]) :- 
+	% change to isNotBarrier([PointH|PointT])?
 	isNotBarrier(FromH,FromT),
 	solve(Point, To, [FromH|FromT]).
 
-solve([FromH|FromT], To, [[FromH|FromT]|Tail]) :- 
-	isNotBarrier(FromH,FromT),
+solve(From, To, [From|[Next|Tail]]) :- 
+	isNotBarrier(From),
+
 	solve(Point, To, Tail).
 
 % subgoal met if point at coordinates (H, W) is not a barrier
+isNotBarrier([H|W]) :- isNotBarrier(H, W).
 isNotBarrier(H,W) :-
 	getLast(W, Width),
 	\+(barrier(H, Width)).
 
 % required for isNotBarrier/2
-getLast([E], E) :- !.
-getLast([_|Tail], E) :- getLast(Tail, E).
+getLast([T], T) :- !.
+getLast([_|Tail], T) :- getLast(Tail, T).
+
+% are 2 points adjacent?
+isAdjacent([H1|[T1]], [H2|[T2]]) :- 
+	isAdjacent([H1|T1], [H2|T2]).
+isAdjacent([H|T1], [H|T1 + 1]).
+isAdjacent([H|T1], [H|T2 - 1]).
+isAdjacent([H|T], [H + 1|T]).
+isAdjacent([H|T], [H - 1|T]).
