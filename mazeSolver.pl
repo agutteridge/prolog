b4db2ad05c29bfdf1d23745d641b54f97372cffa
Alice <alicegutteridge@gmail.com>
2015-02-02 21:56:20 +0000

    subgoal for testing barriers

--- mazeSolver.pl
+++ mazeSolver.pl
@@ -1,9 +1,20 @@
 % solve(List1, List2, List3[Lots of Lists])
 
-solve(From, To, To) :- !.
+solve(Point, [ToH|ToT], [ToH|ToT]) :- !.
 
-solve(Head, To, [Head|[]]) :- 
-	solve(From, To, Head).
+solve([FromH|FromT], To, [[FromH|FromT]|[]]) :- 
+	isNotBarrier(FromH,FromT),
+	solve(Point, To, [FromH|FromT]).
 
-solve(Head, To, [Head|Tail]) :- 
-	solve(From, To, Tail).
+solve([FromH|FromT], To, [[FromH|FromT]|Tail]) :- 
+	isNotBarrier(FromH,FromT),
+	solve(Point, To, Tail).
+
+% subgoal met if point at coordinates (H, W) is not a barrier
+isNotBarrier(H,W) :-
+	getLast(W, Width),
+	\+(barrier(H, Width)).
+
+% required for isNotBarrier/2
+getLast([E], E) :- !.
+getLast([_|Tail], E) :- getLast(Tail, E).
