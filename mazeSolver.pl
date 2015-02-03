13ad9223d920bdabf0bba522c615e6c68db5a5ed
Alice <alicegutteridge@gmail.com>
2015-02-03 17:33:07 +0000

    Separated solve/3 into 2 distinct predicates, creating solveHelper/3. Also added getLast/2 to isAdjacent/2 and created isAdjacent/4.

--- mazeSolver.pl
+++ mazeSolver.pl
@@ -1,19 +1,19 @@
 % solve(List1, List2, List3[Lots of Lists])
 
-solve(From, To, To) :- 
-	isAdjacent(From, To),
+% needed for first call, where first list and head of third list are equal
+solve(From, To, [From|Tail]) :- 
 	isNotBarrier(From),
+	solveHelper(From, To, Tail).
+
+solveHelper(From, To, [To|[]]) :- 
+	isNotBarrier(To),
+	isAdjacent(From, To),
 	!.
 
-solve([FromH|FromT], To, [[FromH|FromT]|[]]) :- 
-	% change to isNotBarrier([PointH|PointT])?
-	isNotBarrier(FromH,FromT),
-	solve(Point, To, [FromH|FromT]).
-
-solve(From, To, [From|[Next|Tail]]) :- 
-	isNotBarrier(From),
-
-	solve(Point, To, Tail).
+solveHelper(From, To, [Next|Tail]) :-
+	isNotBarrier(Next),
+	isAdjacent(From, Next),
+	solveHelper(Next, To, Tail).
 
 % subgoal met if point at coordinates (H, W) is not a barrier
 isNotBarrier([H|W]) :- isNotBarrier(H, W).
@@ -26,9 +26,13 @@ getLast([T], T) :- !.
 getLast([_|Tail], T) :- getLast(Tail, T).
 
 % are 2 points adjacent?
-isAdjacent([H1|[T1]], [H2|[T2]]) :- 
-	isAdjacent([H1|T1], [H2|T2]).
-isAdjacent([H|T1], [H|T1 + 1]).
-isAdjacent([H|T1], [H|T2 - 1]).
-isAdjacent([H|T], [H + 1|T]).
-isAdjacent([H|T], [H - 1|T]).
+isAdjacent([H1|T1], [H2|T2]) :-
+	getLast(T1, N1),
+	getLast(T2, N2),
+	isAdjacent(H1, N1, H2, N2).
+isAdjacent(H, T1, H, T2) :- 
+	T1 is T2 + 1; 
+	T1 is T2 - 1.
+isAdjacent(H1, T, H2, T) :- 
+	H1 is H2 + 1; 
+	H1 is H2 - 1.
